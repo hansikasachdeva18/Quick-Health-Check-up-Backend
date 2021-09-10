c3701c4b2084bee29965324f993b11f8bd9d7ace
hansikasachdeva18 <86574049+hansikasachdeva18@users.noreply.github.com>
2021-09-10 07:39:13 +0530

    Add files via upload

---
+++ ap.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream>]
+using namespace std;
+int main()
+{
+    int a;
+    int b, c;
+    cin >> a >> b >> c;
+    int cd = b - a;
+    cout << cd;
+    return 0;
+}
